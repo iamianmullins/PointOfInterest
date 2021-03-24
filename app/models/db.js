25e78d01cdfa34d32dc5c02540f348b0088960a4
Ian Mullins <mullins.89@hotmail.com>
2021-03-24 21:48:58 +0000

    Seed DB config

--- app/models/db.js
+++ app/models/db.js
@@ -22,3 +22,17 @@ db.on("disconnected", function() {
 db.once("open", function() {
   console.log(`database connected to ${this.name} on ${this.host}`);
 });
+
+async function seed() {
+  var seeder = require('mais-mongoose-seeder')(Mongoose);
+  const data = require('./seed-data.json');
+  const points = require('./points');
+  const User = require('./user');
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
+
+db.once('open', function () {
+  console.log(`database connected to ${this.name} on ${this.host}`);
+  seed();
+});
